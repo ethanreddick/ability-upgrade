fbc43cdb3c59b7d214dac94a752a36e02b04dc3b
Ethan Reddick <ethanreddick55@gmail.com>
2024-02-19 19:27:32 -0500

    Fixed alignment of top menu buttons in navbar, and changed them to no longer have drop-down functionality.:

--- frontend/src/components/SiteNavbar.vue
+++ frontend/src/components/SiteNavbar.vue
@@ -1,77 +1,57 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <!-- Logo on the top left -->
-            <a class="navbar-brand" href="#">Ability Upgrade</a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid d-flex justify-content-between">
+          <!-- Logo on the top left -->
+          <a class="navbar-brand" href="#">Ability Upgrade</a>
 
-            <!-- Centered Navigation Buttons -->
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Menu 1
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink1">
-                            <li><a class="dropdown-item" href="#">Action 1</a></li>
-                            <li><a class="dropdown-item" href="#">Another Action 1</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Menu 2
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
-                            <li><a class="dropdown-item" href="#">Action 2</a></li>
-                            <li><a class="dropdown-item" href="#">Another Action 2</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink3" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Menu 3
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
-                            <li><a class="dropdown-item" href="#">Action 3</a></li>
-                            <li><a class="dropdown-item" href="#">Another Action 3</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-
-            <!-- Social Links on the Top Right -->
-            <div class="d-flex">
-                <a href="https://www.linkedin.com/in/ethanreddick/" class="nav-link">
-                    <font-awesome-icon :icon="['fab', 'linkedin']" class="icon"/>
-                </a>
-                <a href="https://github.com/ethanreddick" class="nav-link">
-                    <font-awesome-icon :icon="['fab', 'github']" class="icon"/>
-                </a>
-            </div>
-        </div>
-    </nav>
+          <!-- Social Links on the Top Right -->
+          <div class="social-links">
+              <a href="https://github.com/ethanreddick" class="nav-link">
+                  <font-awesome-icon :icon="['fab', 'github']" class="icon"/>
+              </a>
+              <a href="https://www.linkedin.com/in/ethanreddick/" class="nav-link">
+                  <font-awesome-icon :icon="['fab', 'linkedin']" class="icon" />
+              </a>
+          </div>
+      </div>
+      <!-- Centered Navigation Buttons, positioned absolutely to be truly centered -->
+      <div class="navbar-nav centered-nav">
+          <a class="nav-item nav-link" href="#">Home</a>
+          <a class="nav-item nav-link" href="#">Blog</a>
+          <a class="nav-item nav-link" href="#">Projects</a>
+      </div>
+  </nav>
 </template>
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
-    name: "SiteNavbar",
-    components: {
-        FontAwesomeIcon
-    }
+  name: "SiteNavbar",
+  components: {
+    FontAwesomeIcon
+  }
 };
 </script>
 
 <style scoped>
 .icon {
-  color: black; /* Sets the icon color to black */
-  font-size: 24px; /* Increase the size of the icon */
-  margin-right: 15px; /* Adds margin to the right of each icon */
+  color: black;
+  font-size: 24px;
+  margin-right: 15px;
 }
-/* If you want to specifically target only the last icon to not have a margin, you can use :last-child selector */
 .nav-link:last-child .icon {
-  margin-right: 0; /* Removes margin for the last icon */
+  margin-right: 0;
+}
+.social-links {
+  display: flex;
+}
+.centered-nav {
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  top: 10px;
+  left: 0;
 }
 </style>
