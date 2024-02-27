b11d6fbb9468293f8e8fc2e527f89dad93cc27ff
Ethan Reddick <ethanreddick55@gmail.com>
2024-02-26 20:19:22 -0500

    Fixed formatting issues on Projects page and added a second project.

--- frontend/src/components/ProjectsPage.vue
+++ frontend/src/components/ProjectsPage.vue
@@ -2,16 +2,17 @@
     <div class="projects-container">
         <div class="project-bubble" v-for="project in projects" :key="project.id" @click="goToProject(project.id)">
             <div class="project-image">
-                <!-- Update :src to use require for dynamic path resolution -->
                 <img :src="require(`@/assets/${project.imageName}`)" alt="Project Image" />
             </div>
-            <div class="project-info">
-                <h3>{{ project.title }}</h3>
-                <p>{{ project.description }}</p>
-            </div>
-            <div class="project-dates">
-                <p>Created: {{ project.created }}</p>
-                <p>Updated: {{ project.updated }}</p>
+            <div class="project-text">
+                <div class="project-info">
+                    <h3>{{ project.title }}</h3>
+                    <p>{{ project.description }}</p>
+                </div>
+                <div class="project-dates">
+                    <p class="created-date">Created: {{ project.created }}</p>
+                    <p class="updated-date">Updated: {{ project.updated }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -27,23 +28,29 @@ export default {
                     id: 1,
                     title: 'Network Attached Storage (NAS)',
                     description: 'A multipurpose network storage server with file sharing, multimedia streaming, and cloud synchronization capabilities.',
-                    imageName: 'nas.png',
+                    imageName: 'nas.png', // image credit: https://www.flaticon.com/free-icon/nas_4943821
                     created: 'July 20th, 2023',
                     updated: 'January 23rd, 2024',
                 },
-                // Add more projects as needed
+                {
+                    id: 2,
+                    title: 'Rasberry Pi 5',
+                    description: 'Used to host this website. I SSH into the Pi using certificates I configured to update the website and manage the server.',
+                    imageName: 'pi.png', // image credit: https://iconduck.com/icons/14184/raspberry-pi
+                    created: 'Febrary 21st, 2024',
+                    updated: 'Febrary 24th, 2024',
+                },
+                // Add more projects here
             ],
         };
     },
     methods: {
         goToProject(projectId) {
-            // Logic to navigate to the project's detailed page
             this.$router.push({ name: 'ProjectDetail', params: { id: projectId } });
         },
     },
 };
 </script>
-
 
 <style scoped>
 .projects-container {
@@ -54,26 +61,47 @@ export default {
 
 .project-bubble {
     display: flex;
+    flex-direction: row;
     align-items: center;
-    margin: 20px;
+    /* Changed from flex-start to center */
+    margin: 10px;
+    /* Decreased from 20px to reduce space between bubbles */
     padding: 20px;
+    /* This applies padding equally on all sides */
+    padding-bottom: 10px;
+    /* Reduced the bottom padding specifically */
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     cursor: pointer;
     border-radius: 15px;
 }
 
-
 .project-image img {
     width: 100px;
-    /* Adjust as needed */
     height: auto;
     margin-right: 20px;
-    border-radius: 15px;
+}
+
+.project-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-grow: 1;
+    max-width: 500px;
 }
 
 .project-info,
 .project-dates {
-    margin-right: 20px;
-    max-width: 450px;
+    margin: 0;
+}
+
+.project-dates {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.project-dates p {
+    color: gray;
+    font-size: 0.8rem;
 }
 </style>
