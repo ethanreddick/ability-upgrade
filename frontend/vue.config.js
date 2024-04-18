f4ea1b6293d952eeb3bc8c86fab9ee0b4f8c149e
Ethan Reddick <ethanreddick55@gmail.com>
2024-04-18 16:27:25 -0400

    Re-modified vue.config to use the regenerated certificate and key

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+const fs = require("fs");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    https: {
+      key: fs.readFileSync("./server.key"),
+      cert: fs.readFileSync("./server.cert"),
+    },
     proxy: {
       "/api": {
         target: "http://localhost:8080",
